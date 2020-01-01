5187a8a5e925fc358f98a11b4886a129f82edf94
sekim20 <42365781+sekim20@users.noreply.github.com>
2020-01-01 12:13:04 +0700

    Add files via upload

---
+++ G_Drive/src/robot-config.cpp
@@ -0,0 +1,33 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor Arm = motor(PORT1, ratio36_1, false);
+motor IntakeLeft = motor(PORT2, ratio18_1, false);
+motor IntakeRight = motor(PORT3, ratio18_1, true);
+motor Tilter = motor(PORT10, ratio36_1, true);
+motor FrontLeft = motor(PORT12, ratio18_1, true);
+motor BackLeft = motor(PORT11, ratio18_1, false);
+motor FrontRight = motor(PORT18, ratio18_1, false);
+motor BackRight = motor(PORT19, ratio18_1, true);
+controller Controller1 = controller(primary);
+gyro Gyro = gyro(Brain.ThreeWirePort.H);
+
+// VEXcode generated functions
+
+
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Text.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
