14419a6eba5995e9c93149726134944c8db98a12
The_Lord_Inquisitor <Son.mofa415@gmail.com>
2020-01-01 11:21:09 +0700

    Jaden

--- vexcode-projects/Random/LiveCoding/src/robot-config.cpp
+++ vexcode-projects/Random/LiveCoding/src/robot-config.cpp
@@ -13,6 +13,10 @@ motor R1 = motor(PORT2, ratio18_1, true);
 controller Controller1 = controller(primary);
 motor L2 = motor(PORT3, ratio18_1, false);
 motor R2 = motor(PORT5, ratio18_1, true);
+motor LeftDriveSmart = motor(PORT11, ratio18_1, false);
+motor RightDriveSmart = motor(PORT12, ratio18_1, true);
+gyro TurnGyroSmart = gyro(Brain.ThreeWirePort.C);
+smartdrive Drivetrain= smartdrive(LeftDriveSmart, RightDriveSmart, TurnGyroSmart, 319.19, 320, 130, mm, 1);
 
 // VEXcode generated functions
 
@@ -24,5 +28,19 @@ motor R2 = motor(PORT5, ratio18_1, true);
  * This should be called at the start of your int main function.
  */
 void vexcodeInit( void ) {
-  // nothing to initialize
+  Brain.Screen.print("Device initialization...");
+  Brain.Screen.setCursor(2, 1);
+  // calibrate the drivetrain gyro
+  wait(200, msec);
+  TurnGyroSmart.calibrate();
+  Brain.Screen.print("Calibrating Gyro for Drivetrain");
+  // wait for the gyro calibration process to finish
+  while (TurnGyroSmart.isCalibrating()) {
+    wait(25, msec);
+  }
+  // reset the screen now that the calibration is complete
+  Brain.Screen.clearScreen();
+  Brain.Screen.setCursor(1,1);
+  wait(50, msec);
+  Brain.Screen.clearScreen();
 }
